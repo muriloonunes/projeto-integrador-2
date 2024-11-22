d0012482999a8cabb60f9e30e04c60268f0ca707
Murilo <murilo_no@outlook.com>
2024-11-22 16:25:32 -0300

    implementaçao parcial de uma tela para digitar o cpf

--- src/main/java/hmd/teatroABC/controller/TelaInicialController.java
+++ src/main/java/hmd/teatroABC/controller/TelaInicialController.java
@@ -1,16 +1,17 @@
 package hmd.teatroABC.controller;
 
 import hmd.teatroABC.model.Peca;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * @author Davy Lopes, Murilo Nunes, Hartur Sales
@@ -33,9 +34,7 @@ public class TelaInicialController {
     }
 
     public void addImagens(Stage stage) {
-        stage.getScene().getWindow().setOnShowing(event -> {
-            imagem1.setImage(peca1.getPosterImg());
-        });
+        stage.getScene().getWindow().setOnShowing(event -> imagem1.setImage(peca1.getPosterImg()));
     }
 
     public void pecaSelecionada() {
@@ -50,11 +49,43 @@ public class TelaInicialController {
         compraStage.show();
     }
 
-    public void imprimirIngressoTrigger() {
+    public void imprimirIngressoTrigger() throws IOException {
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Digite seu CPF");
+        dialog.setHeaderText("Por favor, insira seu CPF para continuar.");
+        dialog.setGraphic(null);
+        dialog.setContentText("CPF:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        final Button okButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
+        okButton.addEventFilter(ActionEvent.ACTION, ae -> {
+            String input = dialog.getEditor().getText();
+            System.out.println("evento");
+            if (!isInvalid(input)) {
+                ae.consume(); //not valid
+            }
+        });
+
+        if (result.isPresent()) {
+            String cpf = result.get();
+            System.out.println("CPF digitado: " + cpf);
+
+            //imprimirIngresso(cpf);
+        } else {
+            System.out.println("Operação cancelada.");
+        }
         //TODO: Implementar a lógica de impressão do ingresso
     }
 
     public void verEstatisticasTrigger() {
         //TODO: Implementar a lógica de visualização de estatísticas
+    }
+
+    private boolean isInvalid(String input) {
+        if (input != null && input.matches("\\d{11}")) {
+            return false; // Retorna o CPF válido
+        }
+        return input == null || !input.equalsIgnoreCase("admin");
     }
 }
